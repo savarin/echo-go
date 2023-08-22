cf711c89aa5581ca8d3d9e35ac68359d1ace068f
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-08-22 15:55:54 -0700

    introduce echo service

---
+++ src/echo_service.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// EchoService struct to provide echoing functionality
+type EchoService struct {
+}
+
+// Echo method takes a message string as input and returns a transformed
+// message containing the length followed by the original message.
+func (e *EchoService) Echo(message string) string {
+	return fmt.Sprintf("%d:%s", len(message), message)
+}
---
+++ src/echo_service_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestEchoShouldReturnTheSameMessage(t *testing.T) {
+	// Arrange: Initialize EchoService and define the expected message
+	echoService := &EchoService{}
+	message := "Hello, World!"
+
+	// Act: Invoke the echo method with the input message
+	result := echoService.Echo(message)
+
+	// Assert: Verify that the result is equal to the expected message
+	expectedResult := "13:" + message
+	if result != expectedResult {
+		t.Errorf("Expected %s, but got %s", expectedResult, result)
+	}
+}
